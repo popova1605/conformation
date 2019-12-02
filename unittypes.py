6a9404e4e92c09a0640c40f933f1380d0abf0902
Cv22 <sidorenko.artem@phystech.edu>
2019-12-02 20:04:26 +0300

    Доработал скелет, необходимо найти спрайты для движения, атаки, смерти.

--- unittypes.py
+++ unittypes.py
@@ -4,14 +4,77 @@
 
 from tkinter import*
 
-def cost(unit_type):
+
+def mana_cost(unit_type):
     """Стоимость"""
-    if unit_type == "unknown_type":
+    if unit_type == "":
         return 0
 
-def card_image(unit_type, canvas, coords):
+
+def attack(unit_type):
     """Объект картинки на карте"""
-    if unit_type == "unknown_type":
-        image = [canvas.create_oval(0, 0, 0, 0)]
+    if unit_type == "":
+        image = ['rogue_run_1_0.png', 'rogue_run_1_1.png', 'rogue_run_1_2.png', 'rogue_run_1_3.png', 'rogue_run_1_4.png',
+                 'rogue_run_1_5.png']
     return image
+
+
+def death(unit_type):
+    if unit_type == "":
+        image = []
+    return image
+
+
+def walk(unit_type):
+    if unit_type == "":
+        image = []
+    return image
+
+
+def card(unit_type):
+    if unit_type == "":
+        image = 0
+    return image
+
+
+def health(unit_type):
+    if unit_type == "":
+        health = 0
+    return health
+
+
+def force(unit_type):
+    if unit_type == "":
+        force = 0
+    return force
+
+
+def step(unit_type):
+    if unit_type == "":
+        step = 0
+    return step
+
+
+def money_cost(unit_type):
+    if unit_type == "":
+        return 0
+
+
+def x(unit_type):
+    if unit_type == "":
+        x = 0
+    return x
+
+
+def additional_mana(unit_type):
+    if unit_type == "":
+        additional_mana = 0
+    return additional_mana
+
+
+def additional_money(unit_type):
+    if unit_type == "":
+        additional_money = 0
+    return additional_money
+
 # etc.
