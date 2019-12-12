6914e89e5425c233a6c9f3c1914d47961eef680c
popova1605 <poel605@yandex.ru>
2019-12-12 15:28:31 +0300

    Добавлен шаблон для сохранения последней игры в файл

---
+++ input.py
@@ -0,0 +1,8 @@
+
+def save_game(game, buttle_field):
+    """Сохраняет текущее состояние игры в файл last_game.txt"""
+    pass
+
+def read_game(game, buttle_field):
+    """Заполняет поля классов значениями из файла last_game"""
+    pass
